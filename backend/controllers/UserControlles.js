f4df3d10f34b57c42246780bce461921d9d08f68
Yuri <yurifdev@gmail.com>
2023-02-25 20:35:52 -0300

    Setting userController

---
+++ backend/controllers/UserControlles.js
@@ -0,0 +1,23 @@
+//Fazendo autentificação
+const User = require("../models/User");
+
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const jwtSecret = process.env.JWT_SECRET;
+
+//Generate user token
+const generateToken = (id) => {
+  return jwt.sign({ id }, jwtSecret, {
+    expiresIn: "7d", //Fazendo logout automático depois de 7dias
+  });
+};
+
+//Register user and sign in
+const register = async (req, res) => {
+  res.send("Registro");
+};
+
+module.exports = {
+    register, 
+}
