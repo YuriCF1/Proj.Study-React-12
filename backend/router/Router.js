ab8c3718485d61bdcc5a6d36ac081383fe2fa79e
Yuri <yurifdev@gmail.com>
2023-02-26 17:18:30 -0300

    Setting router of registration

--- backend/router/Router.js
+++ backend/router/Router.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express();
 
+//Importing router
+router.use("/api/users", require("./UserRoutes"));
+
 // Text route
 router.get("/", (req, res) => {
   res.send("API WORKING");
