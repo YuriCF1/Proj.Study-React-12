97256ce9e89488ea7ffc21be92b7a4d4b4008d83
Yuri <yurifdev@gmail.com>
2023-02-27 11:49:55 -0300

    Importing the validation of the field on body message

--- backend/router/UserRoutes.js
+++ backend/router/UserRoutes.js
@@ -4,7 +4,11 @@ const router = express.Router();
 //Controller, importando
 const { register } = require("../controllers/UserControlles");
 
+//Middlewares
+const validate = require("../middlewares/handleValidation");
+const { userCreateValidation } = require("../middlewares/usersValidation");
+
 //Routes
-router.post("/register", register);
+router.post("/register", userCreateValidation(), validate, register);
 
 module.exports = router;
