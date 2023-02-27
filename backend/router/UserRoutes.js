14e271d96c8cdbfe969b6fa9c011398da605cad8
Yuri <yurifdev@gmail.com>
2023-02-27 20:43:40 -0300

    Adding the loginValidation to the make a login

--- backend/router/UserRoutes.js
+++ backend/router/UserRoutes.js
@@ -2,13 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 //Controller, importando
-const { register } = require("../controllers/UserControlles");
+const { register, login } = require("../controllers/UserControlles");
 
 //Middlewares
 const validate = require("../middlewares/handleValidation");
-const { userCreateValidation } = require("../middlewares/usersValidation");
+const { userCreateValidation, loginValidation } = require("../middlewares/usersValidation");
 
 //Routes
 router.post("/register", userCreateValidation(), validate, register);
+router.post("/login", loginValidation(), validate, login);
 
 module.exports = router;
