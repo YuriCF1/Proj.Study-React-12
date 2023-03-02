b6fcf8c50101647319eca168e6902e34b213b078
Yuri <yurifdev@gmail.com>
2023-03-01 23:52:14 -0300

    Adding the authGuard method and getCurrentUser

--- backend/router/UserRoutes.js
+++ backend/router/UserRoutes.js
@@ -2,14 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 //Controller, importando
-const { register, login } = require("../controllers/UserControlles");
+const { register, login, getCurrentUser } = require("../controllers/UserControlles");
 
 //Middlewares
 const validate = require("../middlewares/handleValidation");
 const { userCreateValidation, loginValidation } = require("../middlewares/usersValidation");
+const authGuard = require("../middlewares/authGuard")
 
 //Routes
 router.post("/register", userCreateValidation(), validate, register);
 router.post("/login", loginValidation(), validate, login);
-
+router.get("/profile", authGuard, getCurrentUser)
 module.exports = router;
