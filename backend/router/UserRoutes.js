2ccb125ead733ae65c6bfe0e49cbbc4a66d62a2e
Yuri <yurifdev@gmail.com>
2023-03-03 17:42:52 -0300

    Setting user update route with its methods

--- backend/router/UserRoutes.js
+++ backend/router/UserRoutes.js
@@ -2,15 +2,36 @@ const express = require("express");
 const router = express.Router();
 
 //Controller, importando
-const { register, login, getCurrentUser } = require("../controllers/UserControlles");
+const {
+  register,
+  login,
+  getCurrentUser,
+  update,
+} = require("../controllers/UserControlles");
 
 //Middlewares
 const validate = require("../middlewares/handleValidation");
-const { userCreateValidation, loginValidation } = require("../middlewares/usersValidation");
-const authGuard = require("../middlewares/authGuard")
+const {
+  userCreateValidation,
+  loginValidation,
+  userUpdateValidation,
+} = require("../middlewares/usersValidation");
+const authGuard = require("../middlewares/authGuard");
+const { imageUpload } = require("../middlewares/imageUpload");
 
 //Routes
 router.post("/register", userCreateValidation(), validate, register);
 router.post("/login", loginValidation(), validate, login);
-router.get("/profile", authGuard, getCurrentUser)
+router.get("/profile", authGuard, getCurrentUser);
+router.put(
+  "/",
+  authGuard,
+  userUpdateValidation(),
+  validate,
+  imageUpload.single("profileImage"),
+  update
+); //put = verbo de atualização
+//Salvando na coleção 'profileImage na requisição, que envia o nome do arquivo
+//O método single() do multer é responsável por definir o nome do campo que será utilizado para fazer o upload do arquivo.
+
 module.exports = router;
