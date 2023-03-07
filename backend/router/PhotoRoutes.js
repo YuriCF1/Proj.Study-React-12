1077706b9ff89528fabe59aeb9a6e8d630da5d4d
Yuri <yurifdev@gmail.com>
2023-03-07 11:14:35 -0300

    Setting middlewares of to validade and create photo

--- backend/router/PhotoRoutes.js
+++ backend/router/PhotoRoutes.js
@@ -1,13 +1,23 @@
 const express = require("express");
-const router = express.router;
+const router = express.Router();
 
-//AbortController
+//Controller
+const { insertPhoto } = require("../controllers/PhotoController");
 
 //Middlewares
-const { photoInservalidation } = require("../middlewares/photoValidation");
+const { photoInsertValidation } = require("../middlewares/photoValidation");
 const authGuard = require("../middlewares/authGuard");
 const validate = require("../middlewares/handleValidation");
+const { imageUpload } = require("../middlewares/imageUpload");
 
 //Routes
+router.post(
+  "/",
+  authGuard,
+  imageUpload.single("image"),
+  photoInsertValidation(),
+  validate,
+  insertPhoto,
+);
 
 module.exports = router;
