26c2d5180d751a25c8358310bd74e48274a2fb83
Yuri <yurifdev@gmail.com>
2023-03-07 10:59:26 -0300

    Setting model of photo

--- backend/models/Photo.js
+++ backend/models/Photo.js
@@ -1,4 +1,4 @@
-const mongoose = require("mangoose");
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const photoSchema = new Schema(
@@ -15,6 +15,12 @@ const photoSchema = new Schema(
   }
 );
 
-const Photo = mangoose.module("Photo", photoSchema);
+//O primeiro parâmetro é o nome da coleção, que será usado para criar a coleção no banco de dados, com o primeiro caractere em minúsculo e em plural 
+//(no caso, "Photo" seria transformado em "photos"). 
+//O segundo parâmetro é o schema que será usado para validar e definir a estrutura dos documentos que serão armazenados na coleção.
+const Photo = mongoose.mongoose.model("Photo", photoSchema);
 
 module.exports = Photo;
+
+//Com esse modelo, podemos usar os métodos disponíveis no Mongoose para interagir com os documentos da coleção, como criar, atualizar, remover e buscar. 
+//Além disso, podemos utilizar o modelo para criar instâncias de fotos no banco de dados e fazer as operações necessárias em cada instância, como salvar ou atualizar informações.
