1a70facba3c623374b177069950846298ce15b83
Yuri <yurifdev@gmail.com>
2023-03-14 18:51:12 -0300

    Refactoring sequence and commenting

--- backend/router/PhotoRoutes.js
+++ backend/router/PhotoRoutes.js
@@ -8,12 +8,16 @@ const {
   getAllPhotos,
   getUserPhotos,
   getPhotoById,
+  updatePhoto,
 } = require("../controllers/PhotoController");
 
 //Middlewares
-const { photoInsertValidation } = require("../middlewares/photoValidation");
+const {
+  photoInsertValidation,
+  photoUpdateValidation,
+} = require("../middlewares/photoValidation");
 const authGuard = require("../middlewares/authGuard");
-const validate = require("../middlewares/handleValidation");
+const validate = require("../middlewares/handleValidation"); //Valida se foi retornado erros, de acordo com od  middlewares antecessores
 const { imageUpload } = require("../middlewares/imageUpload");
 
 //Routes
@@ -28,7 +32,8 @@ router.post(
 
 router.delete("/:id", authGuard, deletePhoto);
 router.get("/", authGuard, getAllPhotos);
-router.get("/user/:id", authGuard, getUserPhotos);
-router.get("/:id", authGuard, getPhotoById)
+router.get("/user/:id", authGuard, getUserPhotos); //"Cuidado com a ordem, pois talvez o express reconheça 'user' como id". Porém, mudei a rota e deu certo
+router.get("/:id", authGuard, getPhotoById);
+router.put("/:id", authGuard, photoUpdateValidation(), validate, updatePhoto);
 
 module.exports = router;
