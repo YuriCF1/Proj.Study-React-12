d21876687c28753d6c812d780d2969f790f2146a
Yuri <yurifdev@gmail.com>
2023-03-19 15:32:22 -0300

    Fixing ul tag with li

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,11 +18,13 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <NavBar />
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/register" element={<Register />}></Route>
-        </Routes>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/register" element={<Register />}></Route>
+          </Routes>
+        </div>
         <Footer />
       </BrowserRouter>
     </div>
