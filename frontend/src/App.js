95494d3fecc53402cd20f9857d85f6b5d2e77012
Yuri <yurifdev@gmail.com>
2023-03-19 14:42:39 -0300

    Adding the navbar and footer components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,15 +9,21 @@ import Home from "./pages/Home/Home";
 import Login from "./pages/Auth/Login ";
 import Register from "./pages/Auth/Register";
 
+//Components
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <NavBar />
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/login" element={<Login />}></Route>
           <Route path="/register" element={<Register />}></Route>
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
