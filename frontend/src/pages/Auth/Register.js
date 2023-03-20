c6111c35accc180f49ca87470a93c2d8109dca87
Yuri <yurifdev@gmail.com>
2023-03-19 23:07:11 -0300

    Setting register form

--- frontend/src/pages/Auth/Register.js
+++ frontend/src/pages/Auth/Register.js
@@ -1,11 +1,29 @@
-import React from "react";
-
 import "./Auth.css";
 
+//Components
+import { Link } from "react-router-dom";
+
+//Hooks
+import React, { useEffect, useState } from "react";
+
 const Register = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("enviado");
+  };
+
   return (
     <div>
-      <h1>Register</h1>
+      <h2>ReactGram</h2>
+      <p className="subTitle">Cadastre-se para ver as fotos dos seus amigos</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Nome" />
+        <input type="email" placeholder="E-mail" />
+        <input type="password" placeholder="Senha" />
+        <input type="password" placeholder="Confirma sua senha" />
+        <input type="sbubmit" value="Cadastrar" />
+      </form>
+      <p>Já possui uma conta? <Link to="/login">Clique aqui</Link></p>
     </div>
   );
 };
