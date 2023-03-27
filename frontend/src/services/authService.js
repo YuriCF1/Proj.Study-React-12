610d7fcc1e4271088214786178d55c144bbd5cfb
Yuri <yurifdev@gmail.com>
2023-03-27 11:59:26 -0300

    Console testing

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -3,17 +3,18 @@ import { api, requestConfig } from "../utils/config";
 //Register an user
 const register = async (data) => {
   const config = requestConfig("POST", data);
-  
+
   try {
     const res = await fetch(api + "/users/register", config)
-    .then((res) => res.json())
-    .catch((err) => err);
-    
+      .then((res) => res.json())
+      .catch((err) => err);
+
     if (res) {
       localStorage.setItem("user", JSON.stringify(res));
     }
-    
-    console.log(config);
+
+    console.log("authService", config);
+    console.log("authService", data);
     return res;
   } catch (error) {
     console.log(error);
