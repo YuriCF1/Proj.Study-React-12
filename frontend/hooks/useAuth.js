d48c0cc0a4a964abcb172eabf383b7bcbd5a0356
Yuri <yurifdev@gmail.com>
2023-03-28 23:38:10 -0300

    Creating hook to check if user has authentication

---
+++ frontend/hooks/useAuth.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from "react";
+//CENTRALIZANDO A LÓGICA PARA EU NÃO TER QUE USAR O REDUX EM TODOS OS COMPONENTES
+
+import { useSelector } from "react-redux"; //Para pegar o dado do redux, da store.js do contexto auth
+
+export const useAuth = () => {
+  const { user } = useSelector((state) => state.auth);
+
+  const [auth, setAuth] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (user) {
+      //Vendo se o usuário existe, então quer dizer que ele foi autenticado
+      setAuth(true);
+    } else {
+      setAuth(false); //Até por conta do logout
+    }
+
+    setLoading(false);
+  }, [user]); //Sempre que o usuário for alterado
+};
+
+return { auth, loading };
