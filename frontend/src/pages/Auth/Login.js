7ef534f80ace2cc25e6e2eb052a5d183717239e0
Yuri <yurifdev@gmail.com>
2023-04-02 18:56:54 -0300

    Login page done

--- frontend/src/pages/Auth/Login.js
+++ frontend/src/pages/Auth/Login.js
@@ -9,14 +9,31 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 //Redux
+import { login, reset } from "../../slices/authSlice";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const dispatch = useDispatch();
+  const { loading, error } = useSelector((state) => state.auth);
+
   const handleSubmit = (e) => {
-    e.preventDeafault();
+    e.preventDefault();
+
+    const user = {
+      email,
+      password,
+    };
+
+    dispatch(login(user));
+    console.log("Login", user);
   };
+
+  //Clean all auth states
+  useEffect(() => {
+    dispatch(reset());
+  }, [dispatch]);
 
   return (
     <div id="login">
@@ -28,7 +45,11 @@ const Login = () => {
           placeholder="E-mail"
           onChange={(e) => setEmail(e.target.value)}
           value={email || ""}
-        />{" "}
+        />
+        {error &&
+          error
+            .filter((err) => err.includes("email") || err.includes("encontrado"))
+            .map((err) => <Message key={err} msg={err} type="error" />)}
         {/*Tem que colocar a string vazia, ou vai dizer que o input está sendo mudado antes do tempo*/}
         <input
           type="password"
@@ -36,7 +57,12 @@ const Login = () => {
           onChange={(e) => setPassword(e.target.value)}
           value={password || ""}
         />
-        <input type="submit" placeholder="Entrar" />
+        {error &&
+          error
+            .filter((err) => err.includes("Senha"))
+            .map((err) => <Message key={err} msg={err} type="error" />)}
+        {!loading && <input type="submit" value="Entrar" />}
+        {loading && <input type="submit" disabled value="Aguarde..." />}
       </form>
       <p>
         Não tem uma conta? <Link to="/register">Clique aqui</Link>
