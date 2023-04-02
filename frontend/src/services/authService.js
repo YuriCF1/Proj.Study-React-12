8264e3c6f46a33ad0c534825477d7ccb5bf2b57d
Yuri <yurifdev@gmail.com>
2023-04-01 21:38:58 -0300

    Creating the Login account

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -26,9 +26,34 @@ const logout = () => {
   localStorage.removeItem("user");
 };
 
+//Sign in an user
+const login = async (data) => {
+  //data = user
+  const config = requestConfig("POST", data);
+
+  try {
+    const res = await fetch(api + "users/login", config)
+      .then((res) => res.json)
+      .catch((err) => err);
+
+    if (res) {
+      localStorage.setItem("user", JSON.stringify(res));
+    }
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 const authService = {
   register,
   logout,
+  login,
 };
 
 export default authService;
+
+//NOTAS:
+/*Em resumo, a principal diferença entre res.json() e JSON.stringify() é que o primeiro é específico para o Express e fornece recursos adicionais, 
+como definir o Content-Type do cabeçalho da resposta e lidar com erros de codificação e detecção de JSON. 
+O segundo método é uma funcionalidade padrão do JavaScript para converter objetos em strings JSON, mas não lida com a resposta HTTP em si.*/
