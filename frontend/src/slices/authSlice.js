8c7ef1fe8c874a97007a68853bab1689eccb8cca
Yuri <yurifdev@gmail.com>
2023-04-01 21:52:55 -0300

    Setting the slices of login

--- frontend/src/slices/authSlice.js
+++ frontend/src/slices/authSlice.js
@@ -14,9 +14,10 @@ const initialState = {
 
 //Register an user and sign in
 export const register = createAsyncThunk(
-  "auth/register", //Dando o nome para a função de createAsyncThanks. Padrão ("entidade/ação") Ex: "auth/login"
+  "auth/register", //Dando o nome para a função de createAsyncThanks. Padrão ("entidade/ação") Ex: "auth/login". Para depois ser usado no extrareducers
   async (user, thunkAPI) => {
     const data = await authService.register(user);
+    
     //Check for errors
     if (data.errors) {
       // return thunkAPI.rejectWithValue(data.errors[0]); //Pegando o array de errors do backend que fiz. Posso também pegar todos e colocar em seus devidos inputs
@@ -26,6 +27,23 @@ export const register = createAsyncThunk(
     return data;
   }
 );
+
+//Sign in an user 
+export const login = createAsyncThunk(
+  "auth/login", //Dando o nome para a função de createAsyncThanks. Padrão ("entidade/ação") Ex: "auth/login"
+  async (user, thunkAPI) => {
+    const data = await authService.login(user);
+
+    //Check for errors
+    if (data.errors) {
+      // return thunkAPI.rejectWithValue(data.errors[0]); //Pegando o array de errors do backend que fiz. Posso também pegar todos e colocar em seus devidos inputs
+      return thunkAPI.rejectWithValue(data.errors); //Pegando o array de errors do backend que fiz. Posso também pegar todos e colocar em seus devidos inputs
+    } //1.2 Identificando se tem erros. Vindo da API
+    console.log(data);
+    return data;
+  }
+);
+
 
 //Logout an user
 export const logout = createAsyncThunk("auth/logout", async () => {
@@ -43,6 +61,7 @@ export const authSlice = createSlice({
     },
   },
   extraReducers: (builder) => {
+    //Register
     builder.addCase(register.pending, (state) => {
       state.loading = true;
       state.error = false;
@@ -59,10 +78,28 @@ export const authSlice = createSlice({
       state.error = action.payload; //1.4 Pegando os erros da API e passando para o estado 1.1
       state.user = null;
     });
+    //Logout
     builder.addCase(logout.fulfilled, (state, action) => {
       state.loading = false;
       state.sucess = true;
       state.error = null;
+      state.user = null;
+    });
+    //Login
+    builder.addCase(login.pending, (state) => {
+      state.loading = true;
+      state.error = false;
+    });
+    builder.addCase(login.fulfilled, (state, action) => {
+      state.loading = false;
+      state.sucess = true;
+      state.error = null;
+      state.user = action.payload;
+    });
+    builder.addCase(login.rejected, (state, action) => {
+      //1.3 Req rejeitada.
+      state.loading = false;
+      state.error = action.payload; //1.4 Pegando os erros da API e passando para o estado 1.1
       state.user = null;
     });
   },
