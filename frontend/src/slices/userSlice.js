74a1945a76c932ad653876f81059f784d086b436
Yuri <yurifdev@gmail.com>
2023-04-05 23:00:47 -0300

    Creating slice for the user update

--- frontend/src/slices/userSlice.js
+++ frontend/src/slices/userSlice.js
@@ -20,6 +20,22 @@ export const profile = createAsyncThunk(
   }
 );
 
+//Update user details
+export const updateProfile = createAsyncThunk(
+  "user/update",
+  async (user, thunkAPI) => {
+    const token = thunkAPI.getState().auth.user.token;
+    const data = await userService.updateProfile(user, token);
+
+    //Check for errors
+    if (data.errors) {
+      return thunkAPI.rejectWithValue(data.errors);
+    }
+
+    return data;
+  }
+);
+
 export const userSlice = createSlice({
   name: "user",
   initialState,
@@ -41,7 +57,25 @@ export const userSlice = createSlice({
       state.user = action.payload;
     });
     // Teoricamente, não haverá erros
-    builder.addCase(profile.rejected, (state, action) => {
+    // builder.addCase(profile.rejected, (state, action) => {
+    //   //1.3 Req rejeitada.
+    //   state.loading = false;
+    //   state.error = action.payload; //1.4 Pegando os erros da API e passando para o estado 1.1
+    //   state.user = null;
+    // });
+    builder.addCase(updateProfile.pending, (state) => {
+      state.loading = true;
+      state.error = false;
+    });
+    builder.addCase(updateProfile.fulfilled, (state, action) => {
+      state.loading = false;
+      state.sucess = true;
+      state.error = null;
+      state.user = action.payload;
+      state.message = "Usuário atualizado com sucesso!";
+    });
+    // Teoricamente, não haverá erros
+    builder.addCase(updateProfile.rejected, (state, action) => {
       //1.3 Req rejeitada.
       state.loading = false;
       state.error = action.payload; //1.4 Pegando os erros da API e passando para o estado 1.1
