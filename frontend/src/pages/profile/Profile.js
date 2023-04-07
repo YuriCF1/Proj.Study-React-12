154ad19b0fcf1cf476a9a4db64486ea2e9df61ea
Yuri <yurifdev@gmail.com>
2023-04-06 22:03:27 -0300

    Creating profile folder and starting the component

---
+++ frontend/src/pages/profile/Profile.js
@@ -0,0 +1,32 @@
+// import React from 'react'
+
+//Css
+import Porfile from "./Profile.css";
+
+//Icons
+import {BsFillEyesFill, BsPencilFill, BsXLg} from 'react/icons/bs'
+
+//Files
+import { uploads } from "../../utils/config";
+
+//Components
+import Message from "../../components/Message";
+
+//React-dom
+import { Link, useParams } from "react-router-dom";
+
+//Hooks
+import { useEffect, useRef, useState } from "react";
+
+//Redux
+import { useDispatch, useSelector } from "react-redux";
+
+//Redux
+
+const Profile = () => {
+  return <div>
+    prof
+  </div>;
+};
+
+export default Profile;
