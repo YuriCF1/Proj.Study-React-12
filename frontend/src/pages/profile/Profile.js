fcfab8ef25b419d6945267c7b65874776d89836c
Yuri <yurifdev@gmail.com>
2023-04-08 23:54:21 -0300

    Setting function to get data from user

--- frontend/src/pages/profile/Profile.js
+++ frontend/src/pages/profile/Profile.js
@@ -23,8 +23,42 @@ import { useDispatch, useSelector } from "react-redux";
 
 //Redux
 
+import { getUserDetails } from "../../slices/userSlice";
+
 const Profile = () => {
-  return <div>prof</div>;
+  const { id } = useParams();
+  const dispatch = useDispatch();
+
+  //Another profile
+  const { user, loading } = useSelector((state) => state.user);
+
+  //User itself
+  const { user: userAuth } = useSelector((state) => state.auth);
+
+  //Photo
+
+  //Loading user data
+  useEffect(() => {
+    dispatch(getUserDetails(id));
+  }, [dispatch, id]);
+
+  if (loading) {
+    return <p>Carregando...</p>;
+  }
+
+  return (
+    <div id="profile">
+      <div className="profile-header">
+        {user.profileImage && (
+          <img src={`${uploads}/users/${user.profileImage}`} alt={user.name} />
+        )}
+        <div className="profile-description">
+          <h2>{user.name}</h2>
+          <p>{user.bio}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
