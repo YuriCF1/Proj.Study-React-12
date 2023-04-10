7fd78b4ab08c867435a82a6291e8433e025e56b3
Yuri <yurifdev@gmail.com>
2023-04-09 23:58:49 -0300

    Creating slice of the publish photo function

--- frontend/src/slices/photoSlice.js
+++ frontend/src/slices/photoSlice.js
@@ -11,6 +11,22 @@ const initialState = {
 };
 
 //Functions
+//Publish user photo
+export const publishPhoto = createAsyncThunk(
+  "photo/publish",
+  async (photo, thunkAPI) => {
+    const token = thunkAPI.getState().auth.user.token;
+
+    const data = await photoService.publishPhoto(photo, token);
+
+    //Check erros
+    if (data.errors) {
+      return thunkAPI.rejectWithValue(data.errors[0]);
+    }
+
+    return data;
+  }
+);
 
 export const photoSlice = createSlice({
   name: "photo",
@@ -19,6 +35,27 @@ export const photoSlice = createSlice({
     resetMessage: (state) => {
       state.message = null;
     },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(publishPhoto.pending, (state) => {
+      state.loading = true;
+      state.error = false;
+    });
+    builder.addCase(publishPhoto.fulfilled, (state, action) => {
+      state.loading = false;
+      state.success = true;
+      state.error = null;
+      state.photo = action.payload;
+      state.photos.unshift(state.photo); //Adicionando, no primeiro lugar do array, a foto
+      state.message = "Foto publicada com sucesso!";
+    });
+    builder.addCase(publishPhoto.rejected, (state, action) => {
+      //1.3 Req rejeitada.
+      console.log(state, action);
+      state.loading = false;
+      state.error = action.payload; //1.4 Pegando os erros da API e passando para o estado 1.1
+      state.photo = {};
+    });
   },
 });
 
