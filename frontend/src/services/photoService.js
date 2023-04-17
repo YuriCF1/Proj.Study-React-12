90af9e39bc0236eeb5ead54715521fe90710c541
Yuri <yurifdev@gmail.com>
2023-04-16 23:49:26 -0300

    Update photo function

--- frontend/src/services/photoService.js
+++ frontend/src/services/photoService.js
@@ -46,11 +46,27 @@ const deletePhoto = async (id, token) => {
   }
 };
 
+//Update a photo
+const updatePhoto = async (data, id, token) => {
+  const config = requestConfig("PUT", data, token);
+
+  try {
+    const res = await fetch(api + "/photos/" + id, config)
+      .then((res) => res.json())
+      .catch((err) => err);
+    console.log(res);
+      return res
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 const photoService = {
   publishPhoto,
   getUserPhotos,
   requestConfig,
   deletePhoto,
+  updatePhoto
 };
 
 export default photoService;
