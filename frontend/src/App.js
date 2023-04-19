860aa95347b8a4e1a6e63b2ed3ba792de997d02e
Yuri <yurifdev@gmail.com>
2023-04-19 20:42:18 -0300

    Setting routes of photo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,6 +16,7 @@ import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import EditProfile from "./pages/EditProfile/EditProfile";
 import Profile from "./pages/Profile/Profile";
+import Photo from "./pages/Photo/Photo";
 
 function App() {
   const { auth, loading } = useAuth(); //Já começa como autenticado por conta do slice, pois ele já pega do localStorage antes de fazer o initialState
@@ -38,6 +39,10 @@ function App() {
             <Route
               path="/profile"
               element={auth ? <EditProfile /> : <Navigate to="/login" />}
+            ></Route>
+            <Route
+              path="/photos/:id"
+              element={auth ? <Photo /> : <Navigate to="/login" />}
             ></Route>
             <Route
               path="/users/:id"
