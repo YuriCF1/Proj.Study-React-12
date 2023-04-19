19a2f2f6a055e72d031072e2d2402c769759d248
Yuri <yurifdev@gmail.com>
2023-04-18 23:44:07 -0300

    Creating photo component to show the photo from profile

---
+++ frontend/src/pages/Photo/Photo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./Photo.css";
+
+import { uploads } from "../../utils/config";
+
+const Photo = () => {
+  return <div>Photo</div>;
+};
+
+export default Photo;
