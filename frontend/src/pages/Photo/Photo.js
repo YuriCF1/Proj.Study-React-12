6869039d585a373209af7429c5a011474e1da716
Yuri <yurifdev@gmail.com>
2023-04-19 23:44:16 -0300

    Importing hooks and components

--- frontend/src/pages/Photo/Photo.js
+++ frontend/src/pages/Photo/Photo.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import "./Photo.css";
 
 import { uploads } from "../../utils/config";
+
+//Components
+import Message from "../../components/Message";
+
+//Hooks
+import { Link, useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+
+///Redux
+
 
 const Photo = () => {
   return <div>Photo</div>;
