ccc80a20b8c10212562592e550b07d267fdaeaa5
Yuri <yurifdev@gmail.com>
2023-04-21 23:01:02 -0300

    Setting function to get the photo by id

--- frontend/src/pages/Photo/Photo.js
+++ frontend/src/pages/Photo/Photo.js
@@ -6,16 +6,42 @@ import { uploads } from "../../utils/config";
 
 //Components
 import Message from "../../components/Message";
+import PhotoItem from "../../components/PhotoItem";
 
 //Hooks
 import { Link, useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 
 //Redux
-
+import { getPhotoById } from "../../slices/photoSlice";
 
 const Photo = () => {
-  return <div>Photo</div>;
+  const { id } = useParams();
+  const dispatch = useDispatch();
+
+  const { user } = useSelector((state) => state.auth);
+  const { photo, loading, error, message } = useSelector(
+    (state) => state.photo
+  );
+
+  //Comentários
+
+  //Load photo data
+  useEffect(() => {
+    dispatch(getPhotoById(id));
+  }, [dispatch, id]);
+
+  //Like a coment
+
+  if (loading) {
+    return <p>Carregando...</p>;
+  }
+
+  return (
+    <div id="photo">
+      <PhotoItem photo={photo}/>
+    </div>
+  );
 };
 
 export default Photo;
