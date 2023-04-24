fba43455c0775de9eed5a16438e8bb69d7058ca4
Yuri <yurifdev@gmail.com>
2023-04-23 23:53:48 -0300

    Creating initial files for like component

---
+++ frontend/src/components/LikeContainer.js
@@ -0,0 +1,13 @@
+// import React from 'react'
+
+import "./LikeContainer.css"
+
+import {BsHeart, BsHeartFill} from "react-icons/bs"
+
+const LikeContainer = ({photo, user, handleLike}) => {
+  return (
+    <div>LikeContainer</div>
+  )
+}
+
+export default LikeContainer
