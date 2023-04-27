7ee88ca1c1da7acf05c9402efa42d021027a8b21
Yuri <yurifdev@gmail.com>
2023-04-26 21:35:12 -0300

    Setting coment function and coments structure

--- frontend/src/pages/Photo/Photo.js
+++ frontend/src/pages/Photo/Photo.js
@@ -18,6 +18,7 @@ import {
   getPhotoById,
   likeAPhoto,
   resetMessage,
+  commentingAPhoto,
 } from "../../slices/photoSlice";
 
 import LikeContainer from "../../components/LikeContainer";
@@ -33,7 +34,24 @@ const Photo = () => {
     (state) => state.photo
   );
 
-  //Comentários
+  //Coments
+  const [commentText, setComentText] = useState("");
+
+  const handleComent = (e) => {
+    e.preventDefault();
+
+    const comentData = {
+      comment: commentText,
+      id: photo._id,
+    };
+
+    dispatch(commentingAPhoto(comentData));
+
+    setComentText("");
+
+    resetMessage();
+    console.log("Enviado");
+  };
 
   //Load photo data
   useEffect(() => {
@@ -42,7 +60,8 @@ const Photo = () => {
   }, [dispatch, id]);
 
   console.log(photo);
-  
+
+  //Inserting a like
   const handleLike = () => {
     dispatch(likeAPhoto(photo._id));
     resetMessageHook();
@@ -64,6 +83,40 @@ const Photo = () => {
       <div className="message-container">
         {error && <Message msg={message} type="error" />}
         {message && <Message msg={message} type="sucess" />}
+        {/* Coments area */}
+        {photo.comments && (
+          <div className="comments">
+            <h3>Comentários:({photo.comments.length})</h3>
+            <form onSubmit={handleComent}>
+              <input
+                type="text"
+                placeholder="Insira seu comentário"
+                onChange={(e) => setComentText(e.target.value)}
+                value={commentText || ""}
+              />
+              <input type="submit" value="Enviar" />
+            </form>
+            {photo.comments.length === 0 && <p>Não há comentários ainda</p>}
+            {photo.comments.map((comment) => (
+              <div className="coment" key={comment.comment}>
+                {/* Su eu precisar de id verdadeiramente únicos, utilizar a biblioteca uuid */}
+                <div className="author">
+                  {comment.userImage && (
+                    <img
+                      src={`${uploads}/users/${comment.userImage}`}
+                      alt={comment.userNam}
+                    />
+                  )}
+
+                  <Link to={`/users/${comment.userId}`}>
+                    <p>{comment.userName}</p>
+                  </Link>
+                </div>
+                <p className="coment">{comment.comment}</p>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
