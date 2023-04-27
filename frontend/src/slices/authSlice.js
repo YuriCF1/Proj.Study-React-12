b52960bf04a3fc757ca3fa1d35186aa8f4fb5aa9
Yuri <yurifdev@gmail.com>
2023-04-27 20:49:10 -0300

    Taking notes

--- frontend/src/slices/authSlice.js
+++ frontend/src/slices/authSlice.js
@@ -114,3 +114,70 @@ como acessar o estado da aplicação, despachar outras ações e personalizar a 
 Por exemplo, thunkAPI.getState() para acessar o estado atual da aplicação dentro da função assíncrona e atualizá-lo com os dados da resposta da API. 
 thunkAPI.dispatch() para despachar outras ações que atualizam o estado da aplicação com base na resposta da API.
 */
+
+/*NOTAS: CreateAsyncThunk
+Syntax: createAsyncThunk(typePrefix, payloadCreator, options?)
+
+Onde:
+- typePrefix é uma string que será utilizada para gerar os action types associados a esse thunk
+- payloadCreator é uma função assíncrona que retorna uma Promise, responsável por fazer a lógica do thunk e retornar o payload.
+- options (opcional) é um objeto com configurações extras para o createAsyncThunk, como condition, dispatchConditionRejected, extraReducers, entre outros.
+
+O options é um objeto que pode ser passado como o terceiro argumento para createAsyncThunk. 
+Ele permite configurar o comportamento de várias opções do createAsyncThunk, 
+incluindo a forma como as funções fulfilled e rejected são tratadas.
+Aqui está um exemplo de como usar options para configurar a funcionalidade do createAsyncThunk:
+
+Exemplo:
+
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+const fetchUserById = createAsyncThunk(
+  "users/fetchById",
+  async (userId, thunkAPI) => {
+    const response = await fetch(`/api/users/${userId}`);
+    if (!response.ok) {
+      throw new Error("Error fetching user by id");
+    }
+    return response.json();
+  },
+  {
+    condition: (userId, { getState }) => {
+      const { users } = getState();
+      const user = users.entities[userId];
+      if (user && user.loaded) {
+        // Don't fetch the user if we already have it and it's loaded
+        return false;
+      }
+    },
+    dispatchConditionRejection: true,
+    extraReducers: (builder) => {
+      builder.addCase(fetchUserById.pending, (state, action) => {
+        state.loading = true;
+      });
+      builder.addCase(fetchUserById.fulfilled, (state, action) => {
+        state.loading = false;
+        state.entities[action.payload.id] = action.payload;
+      });
+      builder.addCase(fetchUserById.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      });
+    },
+  }
+);
+
+Neste exemplo, o objeto options é usado para configurar o seguinte:
+
+- condition: uma função que determina se a operação de busca deve ser executada. Esta função é chamada antes que a ação seja enviada à API. 
+Se ela retornar false, a operação de busca será ignorada e a ação fulfilled não será chamada. 
+Neste caso, a função condition usa o estado atual para verificar se já possuímos as informações do usuário e, 
+se for esse o caso, não faz outra chamada à API.
+
+- dispatchConditionRejection: se for definido como true, um objeto rejected será despachado com o tipo de ação 
+<thunkPrefix>/rejectedCondition se a função condition retornar false. Se definido como false (padrão), a ação não será despachada.
+
+- extraReducers: uma função que permite adicionar casos extras ao createSlice. Neste exemplo, adicionamos casos extras para manipular as ações pending, fulfilled e rejected.
+Esses são apenas alguns exemplos de como usar o objeto options. Ele também oferece outras opções, como fulfilledSuffix, rejectedSuffix, serializeError, meta e outros.
+
+*/
