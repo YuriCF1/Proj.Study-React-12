e6d4ad02bc546e6ce120d2b378082dfb2339ccbd
Yuri <yurifdev@gmail.com>
2023-04-28 00:12:30 -0300

    Setting home structure

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -6,13 +6,13 @@ import React, { useEffect } from "react";
 
 //Components
 import LikeContainer from "../../components/LikeContainer";
-import photoItem from "../../components/PhotoItem";
+import PhotoItem from "../../components/PhotoItem";
 
 //React Router
 import { Link } from "react-router-dom";
 
 //Custom hook
-import {useResetComponentMessage} from "../../hooks/useResetComponentMessage";
+import { useResetComponentMessage } from "../../hooks/useResetComponentMessage";
 
 //Hooks redux
 import { useSelector, useDispatch } from "react-redux";
@@ -22,13 +22,13 @@ import { getAllPhotos, likeAPhoto, LikeAPhoto } from "../../slices/photoSlice";
 
 const Home = () => {
   const dispatch = useDispatch();
-  const resetMessage = useResetComponentMessage();
+  const resetMessage = useResetComponentMessage(dispatch);
   const { user } = useSelector((state) => state.auth);
 
   const { photos, loading } = useSelector((state) => state.photos);
 
   console.log(photos);
-  
+
   //Load all photos
   useEffect(() => {
     dispatch(getAllPhotos());
@@ -46,8 +46,31 @@ const Home = () => {
   }
 
   return (
-    <div>
-      <h1>Home</h1>
+    <div id="home">
+      {photos &&
+        photos.map((photo) => (
+          <div key={photo._id}>
+            <PhotoItem photo={photo}></PhotoItem>
+            <LikeContainer
+              photo={photo}
+              user={user}
+              handleLike={handleLike}
+            ></LikeContainer>
+            <Link className="btn" to={`/photos/${photo._id}`}>
+              Ver mais
+            </Link>
+          </div>
+        ))}
+
+      {/* Caso não haja fotos */}
+      {photos && photos.length === 0 && (
+        <h2 className="no-photos">
+          Ainda não há fotos publicadas.{" "}
+          <Link to={`/users/${user._id}`}>
+            Clique aqui para ser o primeiro!
+          </Link>
+        </h2>
+      )}
     </div>
   );
 };
