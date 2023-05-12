f008d43f5013b8abad7e935eaadaf52a26a7c685
Yuri <yurifdev@gmail.com>
2023-05-12 18:31:37 -0300

    Adding function of handleDislike

--- frontend/src/components/LikeContainer.js
+++ frontend/src/components/LikeContainer.js
@@ -4,13 +4,13 @@ import "./LikeContainer.css";
 
 import { BsHeart, BsHeartFill } from "react-icons/bs";
 
-const LikeContainer = ({ photo, user, handleLike }) => {
+const LikeContainer = ({ photo, user, handleLike, handleDislike }) => {
   return (
     <div className="like">
       {photo.likes && user && (
         <>
           {photo.likes.includes(user._id) ? (
-            <BsHeartFill />
+            <BsHeartFill onClick={() => handleDislike(photo)} />
           ) : (
             <BsHeart onClick={() => handleLike(photo)} />
           )}
