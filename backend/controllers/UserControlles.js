385b3fe23dbaf0271097be3ac4711de6ccb2ea5a
Yuri <yurifdev@gmail.com>
2023-05-13 23:55:22 -0300

    Adding test value

--- backend/controllers/UserControlles.js
+++ backend/controllers/UserControlles.js
@@ -10,7 +10,8 @@ const jwtSecret = process.env.JWT_SECRET;
 //Generate user token
 const generateToken = (id) => {
   return jwt.sign({ id }, jwtSecret, {
-    expiresIn: "7d", //Fazendo logout automático depois de 7dias
+    expiresIn: "10s", //Fazendo logout automático depois de 7dias
+    // expiresIn: "7d", //Fazendo logout automático depois de 7dias
   });
 };
 
