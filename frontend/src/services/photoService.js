0998e7c875053bc87c882d4bc3df65eab3678735
Yuri <yurifdev@gmail.com>
2023-05-18 18:16:08 -0300

    Getting and sending errors

--- frontend/src/services/photoService.js
+++ frontend/src/services/photoService.js
@@ -128,9 +128,23 @@ const getAllPhotos = async (token) => {
   try {
     const res = await fetch(api + "/photos/", config)
       .then((res) => res.json())
-      .catch((err) => err);
+      // .catch((err) => err);
+
+      // console.log('RESPOSTA SERVICE: ', res);
+      // console.log('ok?: ', res.ok);
+      
+      // Verifica se a resposta tem um status dentro do intervalo 200-299
+      if (!res.ok && typeof(res) !== "object") {
+        console.log('Erro no if service');
+        console.log('RES SERVICE: ', res);
+        throw res;
+        
+        // return res
+      }
+
     return res;
   } catch (error) {
+    console.log('ERRO SERVICE!');
     console.log(error);
   }
 };
