4404836781dd9600b2d4eb8a2a3be82a3d524e1b
Yuri Cruz <yuricruzf@gmail.com>
2023-07-21 13:38:25 -0300

    Fixing path to module

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,7 +1,7 @@
 // export const api = "http://localhost:5000/api";
 // export const uploads = "http://localhost:5000/uploads";
 export const api = "https://reactgram-backend-yurifdev.onrender.com/api";
-export const uploads = "https://reactgram-backend-yurifdev.onrender.com/uplods"; reactgram-backend-yurifdev
+export const uploads = "https://reactgram-backend-yurifdev.onrender.com/uploads"; //reactgram-backend-yurifdev
 
 export const requestConfig = (method, data, token = null, image = null) => {
   let config;
